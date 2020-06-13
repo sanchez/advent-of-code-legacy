89f6a067fd6284c99bbd33a2afeac1274f7cc862
sanchez <dfitz360@gmail.com>
2020-06-13 12:26:56 +1000

    Can now load input files based on the solution tag

---
+++ Sanchez.AOC.Core/InputLoader.fs
@@ -0,0 +1,30 @@
+module Sanchez.AOC.Core.InputLoader
+
+open System.Diagnostics
+open System.IO
+open System.IO
+
+let private inputFolderLocation = "inputs/"
+
+let load () =
+    let stackFrame = new StackFrame(1)
+    let method = stackFrame.GetMethod()
+    let solutionInfo =
+        method.CustomAttributes
+        |> Seq.tryFind (fun x -> x.AttributeType = typeof<SolutionAttribute>)
+        |> Option.defaultWith (fun () -> failwith "Failed to find solution information")
+        
+    let loadedInfo =
+        solutionInfo.Constructor.Invoke(solutionInfo.ConstructorArguments |> Seq.map (fun x -> x.Value) |> Seq.toArray)
+        :?> SolutionAttribute
+    
+    let finalPath = sprintf "inputs/%s/%s-%s-input.txt" loadedInfo.Year loadedInfo.Day loadedInfo.Part
+    let dir = finalPath |> Path.GetDirectoryName
+    if dir |> Directory.Exists |> not then
+        dir |> Directory.CreateDirectory |> ignore
+        
+    if finalPath |> File.Exists |> not then
+        failwith "Input file does not exist"
+        
+    finalPath
+    |> File.ReadAllText
--- Sanchez.AOC.Solutions.2018/Day1.fs
+++ Sanchez.AOC.Solutions.2018/Day1.fs
@@ -3,4 +3,6 @@ module Sanchez.AOC.Solutions._2018.Day1
 open Sanchez.AOC.Core
 
 let [<Solution("2018", "1", "a")>] partA () =
+    let input = InputLoader.load()
+    
     ()
