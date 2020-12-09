94f44dcbf6da268301923ca1a29f960c9a027717
sanchez <dfitz360@gmail.com>
2020-12-09 22:03:08 +1000

    day 2 completed

--- Sanchez.AOC.Solutions.2020/Day1.fs
+++ Sanchez.AOC.Solutions.2020/Day1.fs
@@ -23,3 +23,25 @@ let [<Solution("2020", "1", "a")>] partA () =
 
     left * right
     |> string
+
+let [<Solution("2020", "1", "b")>] partB () =
+    let input =
+        InputLoader.load().Split("\n")
+        |> Seq.map (fun x -> x.Trim())
+        |> Seq.choose (fun x ->
+            match Int32.TryParse(x) with
+            | true, i -> Some i
+            | _ -> None)
+        |> Seq.toArray
+
+
+    input
+    |> Seq.map (fun x ->
+        input |> Seq.map (fun y ->
+            input |> Seq.map (fun z -> (x, y, z))))
+    |> Seq.collect id
+    |> Seq.collect id
+    |> Seq.filter (fun (x, y, z) -> (x + y + z) = 2020)
+    |> Seq.map (fun (x, y, z) -> x * y * z)
+    |> Seq.head
+    |> string
---
+++ Sanchez.AOC.Solutions.2020/Day2.fs
@@ -0,0 +1,42 @@
+﻿module Sanchez.AOC.Solutions._2020.Day2
+
+open System
+open Sanchez.AOC.Core
+
+let [<Solution("2020", "2", "a")>] partA () =
+    let input =
+        InputLoader.load().Split("\n")
+        |> Seq.map (fun x -> x.Trim())
+        |> Seq.filter (fun x -> x.Length > 0)
+        |> Seq.map (sscanf "%d-%d %c: %s")
+        |> Seq.toArray
+
+    let isValid =
+        input
+        |> Seq.filter (fun (min, max, c, password) ->
+            password
+            |> Seq.filter ((=) c)
+            |> Seq.length
+            |> (fun x -> (x >= min) && (x <= max)))
+
+    isValid |> Seq.length |> string
+
+let [<Solution("2020", "2", "b")>] partB () =
+    let input =
+        InputLoader.load().Split("\n")
+        |> Seq.map (fun x -> x.Trim())
+        |> Seq.filter (fun x -> x.Length > 0)
+        |> Seq.map (sscanf "%d-%d %c: %s")
+        |> Seq.toArray
+
+    let isValid =
+        input
+        |> Seq.filter (fun (first, second, c, password) ->
+            let f = password.[first - 1]
+            let s = password.[second - 1]
+            if (f = c) && (s = c) then false
+            elif f = c then true
+            elif s = c then true
+            else false)
+
+    isValid |> Seq.length |> string
