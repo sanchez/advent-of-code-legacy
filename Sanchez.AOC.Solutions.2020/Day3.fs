b608e4b774e40461664992ac9a34e41c9c535feb
sanchez <dfitz360@gmail.com>
2020-12-24 18:55:07 +1000

    Completed day 3 and 4

---
+++ Sanchez.AOC.Solutions.2020/Day3.fs
@@ -0,0 +1,83 @@
+﻿module Sanchez.AOC.Solutions._2020.Day3
+
+open Sanchez.AOC.Core
+
+type PossibleItem =
+    | Tree
+    | Spacing
+
+let inline loadInput () =
+    let input =
+        InputLoader.load().Split("\n")
+        |> Seq.map (fun x -> x.Trim())
+        |> Seq.map (
+            Seq.choose (
+                function
+                | '.' -> Some Spacing
+                | '#' -> Some Tree
+                | _ -> None)
+            >> Seq.toArray)
+        |> Seq.filter (fun x -> x.Length <> 0)
+        |> Seq.toArray
+
+    let getItem (x, y) =
+        if y >= input.Length then None
+        else
+            let row = input.[y]
+            Some row.[x % row.Length]
+
+    getItem
+
+let [<Solution("2020", "3", "a")>] partA () =
+    let getItem = loadInput()
+
+    let movePos (x, y) = (x + 3, y + 1)
+
+    let rec getItems pos =
+        match getItem pos with
+        | Some x -> x::(pos |> movePos |> getItems)
+        | None -> []
+
+    let items = getItems (0, 0)
+
+    let trees =
+        items
+        |> Seq.filter (function
+            | Tree -> true
+            | Spacing -> false)
+        |> Seq.length
+
+    sprintf "%d" trees
+
+let [<Solution("2020", "3", "b")>] partB () =
+    let getItem = loadInput()
+
+    let movePos (mX, mY) (x, y) = (x + mX, y + mY)
+
+    let rec getItems incre pos =
+        match getItem pos with
+        | Some x -> x::(pos |> movePos incre |> getItems incre)
+        | None -> []
+
+    let movements =
+        [
+            (1, 1)
+            (3, 1)
+            (5, 1)
+            (7, 1)
+            (1, 2)
+        ]
+
+    let trees =
+        movements
+        |> Seq.map getItems
+        |> Seq.map ((|>) (0, 0))
+        |> Seq.map (
+            Seq.filter (
+                function
+                | Tree -> true
+                | Spacing -> false)
+            >> Seq.length)
+        |> Seq.reduce (*)
+
+    sprintf "%d" trees
---
+++ Sanchez.AOC.Solutions.2020/Day4.fs
@@ -0,0 +1,143 @@
+﻿module Sanchez.AOC.Solutions._2020.Day4
+
+open System
+open Sanchez.AOC.Core
+
+let isTrue a b =
+    match (a, b) with
+    | (true, true) -> true
+    | _ -> false
+
+type Passport =
+    {
+        BirthYear: string option // byr
+        IssueYear: string option // iyr
+        ExpirationYear: string option // eyr
+        Height: string option // hgt
+        HairColor: string option // hcl
+        EyeColor: string option // ecl
+        PassportId: string option // pid
+        CountryId: string option // cid
+    }
+
+let inline loadData () =
+    let (currentData, history) =
+        InputLoader.load().Split("\n")
+        |> Seq.map (fun x -> x.Trim())
+        |> Seq.fold (fun (currentData, history) x ->
+            if x.Length = 0 then
+                ([], currentData::history)
+            else (x::currentData, history)) ([], [])
+
+    currentData::history
+    |> Seq.filter (fun x -> x.Length <> 0)
+    |> Seq.map (fun x -> String.Join(" ", x))
+    |> Seq.map (fun x -> x.Split(' '))
+    |> Seq.map (
+        Seq.map (fun x ->
+            let kv = x.Split(':')
+            (kv.[0], kv.[1]))
+        >> Map.ofSeq)
+    |> Seq.map (fun x ->
+        {
+            Passport.BirthYear = x |> Map.tryFind "byr"
+            IssueYear = x |> Map.tryFind "iyr"
+            ExpirationYear = x |> Map.tryFind "eyr"
+            Height = x |> Map.tryFind "hgt"
+            HairColor = x |> Map.tryFind "hcl"
+            EyeColor = x |> Map.tryFind "ecl"
+            PassportId = x |> Map.tryFind "pid"
+            CountryId = x |> Map.tryFind "cid"
+        })
+    |> Seq.toArray
+
+let [<Solution("2020", "4", "a")>] partA () =
+    let input =
+        loadData()
+        |> Seq.map (fun x -> [ x.BirthYear; x.IssueYear; x.ExpirationYear; x.Height; x.HairColor; x.EyeColor; x.PassportId])
+        |> Seq.map (Seq.map Option.isSome >> Seq.reduce isTrue)
+        |> Seq.filter id
+
+    let count = input |> Seq.length
+
+    sprintf "%d" count
+
+let validNumberRange lower upper =
+    trySscanf "%d"
+    >> Option.map (
+        function
+        | i when i >= lower && i <= upper -> true
+        | _ -> false)
+    >> Option.defaultValue false
+
+let validBirthYear = validNumberRange 1920 2002
+let validIssueYear = validNumberRange 2010 2020
+let validExpirationYear = validNumberRange 2020 2030
+
+let validHeight input =
+    let cm =
+        trySscanf "%dcm" input
+        |> Option.map (
+            function
+            | i when i >= 150 && i <= 193 -> true
+            | _ -> false)
+            |> Option.defaultValue false
+
+    let inches =
+        trySscanf "%din" input
+        |> Option.map (
+            function
+            | i when i >= 59 && i <= 76 -> true
+            | _ -> false)
+        |> Option.defaultValue false
+
+    match (cm, inches) with
+    | (true, _) | (_, true) -> true
+    | _ -> false
+
+let validHairColor (input: string) =
+    if input.StartsWith("#") then
+        input.[1..]
+        |> Seq.map (
+            function
+            | c when c >= '0' && c <= '9' -> true
+            | c when c >= 'a' && c <= 'f' -> true
+            | _ -> false)
+        |> Seq.fold isTrue true
+    else false
+
+let validEyeColor =
+    function
+    | "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" -> true
+    | _ -> false
+
+let validPassportId (input: string) =
+    if input.Length = 9 then
+        trySscanf "%d" input
+        |> Option.map (fun _ -> true)
+        |> Option.defaultValue false
+    else false
+
+let validInput f (input: string option) =
+    Option.map f input
+    |> Option.defaultValue false
+
+let [<Solution("2020", "4", "b")>] partB () =
+    let input =
+        loadData()
+        |> Seq.map (fun x -> [
+            validInput validBirthYear x.BirthYear
+            validInput validIssueYear x.IssueYear
+            validInput validExpirationYear x.ExpirationYear
+            validInput validHeight x.Height
+            validInput validHairColor x.HairColor
+            validInput validEyeColor x.EyeColor
+            validInput validEyeColor x.EyeColor
+            validInput validPassportId x.PassportId
+        ])
+        |> Seq.map (Seq.reduce isTrue)
+    
+    input
+    |> Seq.filter id
+    |> Seq.length
+    |> sprintf "%d"
